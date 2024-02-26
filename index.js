346408c43e039e28f748be3d4014d415f0eecc78
nstshirotays <50131994+nstshirotays@users.noreply.github.com>
2024-02-26 14:20:34 +0000

    finish

--- index.js
+++ index.js
@@ -3,12 +3,31 @@ const usersRouter = require("./routes/users");
 const app = express();
 const port = 3000;
 
-// ルートパスへのGETリクエストに対してindex.htmlを返す
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.use("/users", usersRouter);
+app.get("/posenet", (req, res) => {
+  res.sendFile(__dirname + "/public/posenet.html");
+});
+
+app.get("/ssd", (req, res) => {
+  res.sendFile(__dirname + "/public/ssd.html");
+});
+
+app.get("/imagenet", (req, res) => {
+  res.sendFile(__dirname + "/public/imagenet.html");
+});
+
+app.get("/face", (req, res) => {
+  res.sendFile(__dirname + "/public/face.html");
+});
+
+app.get("/faceparts", (req, res) => {
+  res.sendFile(__dirname + "/public/faceparts.html");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
